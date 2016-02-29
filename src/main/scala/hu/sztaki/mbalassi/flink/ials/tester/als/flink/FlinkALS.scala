2d290e9d8209e5f5acba60b5d6423932cb9e6f08
Márton Balassi <balassi.marton@gmail.com>
2016-02-29 13:59:58 +0100

    Update for LastFM data

--- src/main/scala/hu/sztaki/mbalassi/flink/ials/tester/als/flink/FlinkALS.scala
+++ src/main/scala/hu/sztaki/mbalassi/flink/ials/tester/als/flink/FlinkALS.scala
@@ -14,8 +14,8 @@ object FlinkALS {
 
     val parsedArgs = ParameterTool.fromPropertiesFile(paramLoc)
     val inputFile = parsedArgs.getRequired("ALSInput")
-    val numCustomers = parsedArgs.getRequired("ALSNumCustomers").toInt
-    val numStores = parsedArgs.getRequired("ALSNumStores").toInt
+//    val numCustomers = parsedArgs.getRequired("ALSNumCustomers").toInt
+//    val numStores = parsedArgs.getRequired("ALSNumStores").toInt
     val iterations = parsedArgs.getRequired("ALSIterations").toInt
     val numFactors = parsedArgs.getRequired("ALSNumFactors").toInt
     val lambda = parsedArgs.getRequired("ALSLambda").toDouble
@@ -24,9 +24,20 @@ object FlinkALS {
 
     val env = ExecutionEnvironment.getExecutionEnvironment
 
-    // Read and parse the input data
-    val input = env.readCsvFile[(Int, Int)](inputFile)
-      .map(pair => (pair._1, pair._2, 1.0))
+    // Read and parse the input data: (timestamp, user, store == artist, 1)
+    val input = env.readCsvFile[(Long, Int, Int, Int)](inputFile, fieldDelimiter = " ")
+      .map(tuple => (tuple._2, tuple._3, 1.0))
+
+    val customers = input.map(_._1).distinct()
+    val stores = input.map(_._2).distinct()
+
+    val numCustomers = customers.count()
+    val numStores = stores.count()
+
+    println("-------------------------")
+    println("NumCustomers: " + numCustomers)
+    println("NumStores: " + numStores)
+    println("-------------------------")
 
     // Create a model using FlinkML
     val model = ALS()
@@ -38,7 +49,7 @@ object FlinkALS {
 
     model.fit(input)
 
-    val test = env.fromCollection(for {x <- 0 to numCustomers; y <- 0 to numStores} yield (x, y))
+    val test = customers cross stores
 //    val res = model.predict(test).print
 
     val res=model.predict(test) //results from the prediction
@@ -46,7 +57,7 @@ object FlinkALS {
     val ratings=res.collect().toArray //transform the dataset to an array
 
     val sp=Spearman
-    val ranks=sp.ranks(ratings, numCustomers, numStores) //compute the ranks per customers
+    val ranks=sp.ranks(ratings, numCustomers.toInt, numStores.toInt) //compute the ranks per customers
 
     val corr=sp.averageCorr(ranks, ranks)
   }
