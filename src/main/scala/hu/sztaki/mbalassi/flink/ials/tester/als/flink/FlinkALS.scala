0917f37ae8408ee169c0c60ee9a4ad595e5c7d4a
Gábor Hermann <code@gaborhermann.com>
2016-09-13 09:57:56 +0200

    wip

--- src/main/scala/hu/sztaki/mbalassi/flink/ials/tester/als/flink/FlinkALS.scala
+++ src/main/scala/hu/sztaki/mbalassi/flink/ials/tester/als/flink/FlinkALS.scala
@@ -1,26 +1,38 @@
 package hu.sztaki.mbalassi.flink.ials.tester.als.flink
 
 import hu.sztaki.mbalassi.flink.ials.tester.als.correlation.Spearman
+import hu.sztaki.mbalassi.flink.ials.tester.utils.Utils
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.api.scala._
-import org.apache.flink.ml.recommendation. ALS
+import org.apache.flink.ml.recommendation.ALS
+import org.apache.flink.util.Collector
 
 import scala.collection.mutable.ArrayBuffer
 
 object FlinkALS {
+
+  case class ALSParams(
+                        iterations: Int,
+                        blocks: Int,
+                        numFactors: Int,
+                        lambda: Double,
+                        implicitPrefs: Boolean,
+                        alpha: Double
+                      )
+
   def main(args: Array[String]) {
 
-    val paramLoc = "src/main/resources/benchmark.properties"
+    val propFile = Utils.parameterCheck(args)
 
-    val parsedArgs = ParameterTool.fromPropertiesFile(paramLoc)
+    val parsedArgs = ParameterTool.fromPropertiesFile(propFile)
     val inputFile = parsedArgs.getRequired("ALSInput")
-//    val numCustomers = parsedArgs.getRequired("ALSNumCustomers").toInt
-//    val numStores = parsedArgs.getRequired("ALSNumStores").toInt
+    val testInputFile = parsedArgs.getRequired("ALSTestInput")
     val iterations = parsedArgs.getRequired("ALSIterations").toInt
     val numFactors = parsedArgs.getRequired("ALSNumFactors").toInt
     val lambda = parsedArgs.getRequired("ALSLambda").toDouble
     val blocks = parsedArgs.getRequired("ALSBlocks").toInt
-    val implicitPrefs = parsedArgs.getRequired("ImplicitPrefs").equals("true")
+    val implicitPrefs = parsedArgs.getRequired("ALSImplicitPrefs").equals("true")
+    val alpha = parsedArgs.getRequired("ALSAlpha").toInt
 
     val env = ExecutionEnvironment.getExecutionEnvironment
 
@@ -31,32 +43,62 @@ object FlinkALS {
     val customers = input.map(_._1).distinct()
     val stores = input.map(_._2).distinct()
 
-    val numCustomers = customers.reduce((x1, x2) => (if(x1 > x2) x1 else x2)).collect()(0)
-    val numStores = stores.reduce((x1, x2) => (if(x1 > x2) x1 else x2)).collect()(0)
+    val numCustomers = customers.reduce((x1, x2) => (if (x1 > x2) x1 else x2)).collect()(0)
+    val numStores = stores.reduce((x1, x2) => (if (x1 > x2) x1 else x2)).collect()(0)
 
     println("-------------------------")
     println("NumCustomers: " + numCustomers)
     println("NumStores: " + numStores)
     println("-------------------------")
+  }
 
-    // Create a model using FlinkML
+  // todo test
+  def allUserItemPairs(data: DataSet[(Int,Int,Double)]): DataSet[(Int,Int)] = {
+    val users = data.map(_._1).distinct()
+    val items = data.map(_._2).distinct()
+
+    users cross items
+  }
+
+  // todo test
+  def notRatedUserItemPairs(data: DataSet[(Int,Int,Double)]): DataSet[(Int,Int)] = {
+    val rated = data.map(x => (x._1,x._2)).distinct()
+    val all = allUserItemPairs(data)
+
+    minus(all, rated)
+  }
+
+  // todo test
+  def minus[A](a: DataSet[A], b: DataSet[A]): DataSet[A] = {
+    a.fullOuterJoin(b).where(x => x).equalTo(x => x).apply(
+      (x: A, y: A, out: Collector[A]) => {
+        if (x != null && y == null) {
+          out.collect(x)
+        }
+      }
+    )
+  }
+
+  def trainAndGetRankings(
+                           train: DataSet[(Int, Int, Double)],
+                           test: DataSet[(Int, Int, Double)],
+                           als: ALSParams
+                         ): DataSet[(Int, Int, Double, Int)] = {
     val model = ALS()
-      .setNumFactors(numFactors)
-      .setIterations(iterations)
-      .setLambda(lambda)
-      .setBlocks(blocks)
-      .setImplicit(implicitPrefs)
+      .setNumFactors(als.numFactors)
+      .setIterations(als.iterations)
+      .setLambda(als.lambda)
+      .setBlocks(als.blocks)
+      .setImplicit(als.implicitPrefs)
+      .setAlpha(als.alpha)
 
-    model.fit(input)
+    model.fit(train)
 
-    val test = customers cross stores
-    val res = model.predict(test)
+    val testToGivePreds = test.map(x => (x._1,x._2))
 
-    val sp=Spearman
+    val predictions = model.predict(testToGivePreds)
 
-      val ranks = sp.ranks(res)
-      val averageCorr = sp.corr(ranks, ranks)
-      println(averageCorr)
+    Spearman.ranks(predictions)
   }
 
 }
--- src/main/scala/hu/sztaki/mbalassi/flink/ials/tester/utils/Utils.scala
+++ src/main/scala/hu/sztaki/mbalassi/flink/ials/tester/utils/Utils.scala
@@ -2,7 +2,7 @@ package hu.sztaki.mbalassi.flink.ials.tester.utils
 
 object Utils {
 
-  def parameterCheck(args: Array[String]): Unit = {
+  def parameterCheck(args: Array[String]): String = {
     def outputNoParamMessage(): Unit = {
       val noParamMsg = "\tUsage:\n\n\t./run <path to paramaters file>"
       println(noParamMsg)
